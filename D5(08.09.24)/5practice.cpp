b95191897793db1443894e1f636d4977b9ca00eb
Balendra Pal <balendrapal615@gmail.com>
2024-09-15 20:20:50 +0530

    Initial 5 days of C++

--- D5(08.09.24)/5practice.cpp
+++ D5(08.09.24)/5practice.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main(){
-    int n;
-    cout<< "Enter n : ";
-    cin>>n;
-    
+    int n=10829;
+    int digSum =0;
+    while(n>0){
+        int lastDig = n%10;
+        //cout << lastDig<< " ";  //to print digits in reverse order
+        digSum += lastDig; 
+        n = n/10;
+    }
+    cout<< "Sum of digitd = "<<digSum<<endl;
     return 0;
 }
